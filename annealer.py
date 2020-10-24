c24533950be99f1b9abadb9d9b1a1aea11233fae
mareksubocz <marsub@wp.pl>
2020-10-24 16:54:26 +0200

    errors fixed

--- annealer.py
+++ annealer.py
@@ -1,19 +1,20 @@
 import numpy as np
 import dwavebinarycsp
-from pyqubo import Binary
+from pyqubo import Binary, Array
 
 def get_qnn_bqm(layers: list, lagrange_propagation=1, stitch_kwargs = None):
     if stitch_kwargs is None:
         stitch_kwargs = {}
     annealer = NeuralNetworkAnnealer(layers)
-    annealer.get_bqm(stitch_kwargs, lagrange_propagation)
+    return annealer.get_bqm(stitch_kwargs=stitch_kwargs, lagrange_propagation=lagrange_propagation)
 
 
 class Network:
     def __init__(self, layers: list):
         self.layers = layers
-        self.weights = [np.random.rand(layers[i], layers[i+1]) * 2 - 1
-                        for i in range(len(layers)-1)]
+        self.weights = [Array(
+                    (np.random.rand(layers[i], layers[i+1]) * 2 - 1).tolist()
+                ) for i in range(len(layers)-1)]
         #TODO: add bias?
         # self.biases = [np.random.rand(s) for s in layers]
 
@@ -24,6 +25,7 @@ class NeuralNetworkAnnealer:
         self.H_vars = set()
         self.network = Network(layers)
 
+
     def get_label(self, layer, num):
         return f"{layer}_{num}"
 
@@ -31,37 +33,37 @@ class NeuralNetworkAnnealer:
     def propagationConstraint(self, network: Network, anneal_inputs=False,
                               lagrange = 1):
         # training data
-        values = np.array([0,1])
-        expected = [1]
+        # TODO: pass training data by argument and process function in loop
+        values = Array([0,1])
+        expected = Array([1,0])
 
         # forward propagation
-        for layer_num, layer in enumerate(network.layers):
+        for layer_num, layer in enumerate(network.layers[:-1]):
+
             # if you want to ensure input layer will not be annealed
             if layer_num == 0 and not anneal_inputs:
                 values = values.dot(network.weights[layer_num])
                 continue
 
-            # TODO: delete zeros from matrices to speed up computation
             qubit_values = []
             nodes = {self.get_label(layer_num, i) for i in range(layer)}
+
+            # TODO: delete zeros from matrices to speed up computation
             for node in nodes:
+                # making sure the variable is in final equation
                 if node not in self.H_vars:
                     x_var = Binary(node)
                     self.H_vars.add(x_var)
                 else:
                     x_var = H_vars[node]
-                qubit_values.append(x_var)
-            qubit_values = np.array(qubit_values)
-            values *= qubit_values
 
+                qubit_values.append(x_var)
+
+            values *= Array(qubit_values)
             values = values.dot(network.weights[layer_num])
 
         # values is output array at this point
-        self.H += ((values - expected)**2).sum()
-
-
-    def sum_to_n_constraint(self):
-        pass
+        self.H += sum((values - expected)*(values - expected))
 
 
     def get_bqm(self, stitch_kwargs = None, lagrange_propagation=1):
@@ -69,11 +71,3 @@ class NeuralNetworkAnnealer:
         self.model = self.H.compile()
         bqm = self.model.to_dimod_bqm()
         return bqm
-
-# # TODO: input pairs to train
-# network = Network([2, 8, 8, 8, 2])
-# neuralNetworkAnnealer = NeuralNetworkAnnealer()
-# neuralNetworkAnnealer.propagationConstraint(network)
-# from pprint import pprint
-# pprint(network.weights)
-
--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 from annealer import get_qnn_bqm
 import tabu
+from pprint import pprint
 
 from dwave.system.composites import EmbeddingComposite
 from dwave.system.samplers import DWaveSampler
@@ -13,24 +14,32 @@ def start_annealing(layers: list, lagrange_propagation=1, qpu=False, stitch_kwar
         sampler = tabu.TabuSampler()
 
     bqm = get_qnn_bqm(layers, lagrange_propagation, stitch_kwargs)
+
     # Check elements in the BQM
-    for q in bqm.linear:
-        if bqm.linear[q] != -1:
-            print(q)
-    for q in bqm.quadratic:
-        if bqm.quadratic[q] != 2:
-            print(q, bqm.quadratic[q])
+    # for q in bqm.linear:
+    #     if bqm.linear[q] != -1:
+    #         print(q)
+    # for q in bqm.quadratic:
+    #     if bqm.quadratic[q] != 2:
+    #         print(q, bqm.quadratic[q])
 
     # Run BQM and get the solution and the energy
     sampleset = sampler.sample(bqm)
     solution1 = sampleset.first.sample
     energy1 = sampleset.first.energy
 
+    print("Nodes chosen ('layer_n.o'): ")
+
+    pprint(solution1)
+    print("Solution's energy: ", energy1)
+
     # Determine which nodes are involved
-    selected_nodes = [k for k, v in solution1.items() if v == 1 and not k.startswith('aux')]
-    from pprint import pprint
-    pprint(selected_nodes, ' Energy ', energy1)
+    # selected_nodes = [k for k, v in solution1.items() if v == 1 and not k.startswith('aux')]
+    # from pprint import pprint
+    # pprint(selected_nodes, ' Energy ', energy1)
 
 if __name__ == "__main__":
-    layers = [2,5,5,5,1]
-    start_annealing(layers)
+    layers = [2,5,2]
+    # qpu: use real qpu instead of local simulator
+    start_annealing(layers, lagrange_propagation=1, qpu=True)
+
